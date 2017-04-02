bc8f82b6ef4571113c71754fa99a4bb46e912c60
mikeila95 <micaak14@gmail.com>
2017-04-02 18:59:33 -0300

    Create menu6pruebaDEFINE.h

---
+++ menu6pruebaDEFINE.h
@@ -0,0 +1,32 @@
+#define Msj_ENTRADA "Bienvenido!\n"
+#define Msj_INICIO "\nIngrese lo que se desea hacer:\n1. Muestrar una funcion predefinida\n0. Salir\n"
+#define Msj_PARAMETROS "\nIngrese el tiempo inicial de muestro, el tiempo final, y la cantidad de muestras:\n"
+#define Msj_MENU "\nSeleccione la funcion que se desea muestrar:\n-1 Seno\n-2 Logaritmo\n-3 Loglineal\n-4 Exponencial\n-5 Heaviside\n-6 Mrua\n-7 Paraboloide Hiperbolico\n-8 Volver al inicio\n\n"
+#define Msj_CIERRE "Fin del programa\n"
+    
+#define MSJ_INI_TI "Ingrese el tiempo inicial: "
+#define MSJ_INI_TF "Ingrese el tiempo final: "
+#define MSJ_INI_MUE "Ingrese la cantidad de muestras: "
+#define MSJ_INI_PRE "Ingrese la precision a utilizar: "
+    
+#define pi 3.14159
+    
+#define MSJ_SEN_FRQ "\nIngrese la frecuencia: "
+#define MSJ_SEN_FAS "\nIngrese la fase: "
+#define MSJ_SEN_AMP "\nIngrese la amplitud: "
+    
+#define MSJ_MRUA_POS "\nIngrese la posicion inicial: "
+#define MSJ_MRUA_VEL "\nIngrese la velocidad inicial: "
+#define MSJ_MRUA_ACEL "\nIngrese la aceleracion: "
+    
+#define MSJ_PAR_XO "\nIngrese el parametro Xo: "
+#define MSJ_PAR_YO "\nIngrese el parametro Yo: "
+#define MSJ_PAR_A "\nIngrese la constante A: "
+#define MSJ_PAR_B "\nIngrese la constante B: "
+#define MSJ_PAR_INTER "\nIngrese el intervalo entre muestras: "
+#define MSJ_PAR_MUES "\nIngrese la cantidad de muestras: "
+#define Msj_ER_Menu "\nIngrese un valor valido\n"
+    
+#define MSJ_PAR_ERPRE "\n ERROR: Ingrese una precision entera mayor a 0\n"
+#define TABLA_XY "\nX|Y|f(x,y)\n"
+#define DISP_PARAB "\nParaboloide Hiperbolico de la forma f(x,y)=(x/a)^2+(y/b)^2\n"
