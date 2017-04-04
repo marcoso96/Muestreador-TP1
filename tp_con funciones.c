375d2f6eb6e30c9d6019ae538b7eb1707ca315b7
Sh4ki <jacquelineoura@gmail.com>
2017-04-03 22:32:23 -0300

    Add files via upload
    ultimas modificaciones del tp

--- tp_con funciones.c
+++ tp_con funciones.c
@@ -2,7 +2,7 @@
 #include<math.h>
 #include<stdlib.h>
 //#include "muestras.h"
-//#include "muestras.c"
+
 
 
 typedef  enum { SALIR=0, INGRESAR} t_bool;
@@ -19,8 +19,10 @@ float funcion_senoidal (float dato, float AMP, float FASE, float FREC);
 void  cargar_datos_senoidal (float AMP, float FASE, float FREC);
 float cargar_seno (t_parametros datos);
 float cargar_mrua (t_mrua dato ) ;
+float cargar_parab ();
 
 float  funcion_mrua(float dato,float  x,float v,float a) ;
+float funcion_phiper(float dato, float z, float a, float b);
 
 
 #define MAX_MUESTRAS  100
@@ -50,6 +52,7 @@ float  funcion_mrua(float dato,float  x,float v,float a) ;
 
 #define FUNCION_SENOIDAL    "v (t)= amplitud * Sen(2 * pi * frecuencia * tiempo + fase)"
 #define FUNCION_MRUA        " x(t)= x+ v*t + 0.5*a*t^2 "
+#define FUNCION_HIPERBOLICA   " /f(t)/ = ( (x*b/a) - z*b^2 )^ 1/2  "
 
 #define MSJ_FASE      "Fase en radianes (Considerando un rango de 0 a 2*pi, (pi=3.1416): "
 #define MSJ_FRECUENCIA  "Frecuencia: "
@@ -60,18 +63,19 @@ float  funcion_mrua(float dato,float  x,float v,float a) ;
 #define MSJ_MRUA_VEL "\nIngrese la velocidad inicial: "
 #define MSJ_MRUA_ACEL "\nIngrese la aceleracion: "
 
+#define MSJ_VALOR        "Ingresar el valor:  "
+
+#define VALOR_Z       "\n\nIngrese valor de 'z' :  "
+#define VALOR_A       "\n\nIngrese valor de 'a' :  "
+#define VALOR_B       "\n\nIngrese valor de 'b' :  "
 
 
 
-
-
-// #define FUNCION_EXP
 
 
 int main (void)
-{ //double suma=0;
-  //double muestras[MAX_MUESTRAS];
- // double coef[GRADO+1];
+{
+
   t_bool opcion ;
   float t_inicial, t_final, base;
   size_t i;
@@ -85,139 +89,167 @@ int main (void)
 
 
 /************************* MENU ***************************************/
-do{
-repetir=1;
-fprintf(stderr,"%s\n\n%s\n%s\n\n\n", MSJ_MENU, MSJ_MUESTREAR, MSJ_SALIR );
 
-scanf("%d", &opcion) ;
 
-if( opcion!=SALIR && opcion!=INGRESAR)
-    {
-      fprintf(stderr,"error: ingreso mal la opcion\n");
-      return EXIT_FAILURE;
-    } ;
+    do{
+        repetir=1;
+        fprintf(stderr,"%s\n\n%s\n%s\n\n\n", MSJ_MENU, MSJ_MUESTREAR, MSJ_SALIR );
 
-if ( opcion==SALIR)
+        scanf("%d", &opcion) ;
+
+        if( opcion!=SALIR && opcion!=INGRESAR)
+            {
+                fprintf(stderr,"error: ingreso mal la opcion\n");
+                return EXIT_FAILURE;
+            } ;
+
+        if ( opcion==SALIR)
                 return EXIT_FAILURE ;
 
 
-                /*ENTRADA*/
-do{
-    while(getchar()!='\n');
-    fprintf(stderr,"%s",MSJ_T_INICIAL);
-    j=scanf("%f",&t_inicial);
-    if( j==0 ){
-            fprintf(stderr,"error: ingreso mal el dato\n");
-                 }
-    else  j =1;
-}
-while( j==0);
 
-/*al salir del ciclo se obtendra un valor de tiempo mayor o igual a cero ya que el tiempo*/
-/*se introduce el valor minimo de tiempo con el que se trabajara*/
-do{
-    while(getchar()!='\n');
-    fprintf(stderr,"%s",MSJ_T_FINAL);
-    j=scanf("%f",&t_final);
-    if( j==0 || t_final<=t_inicial ){
-            fprintf(stderr,"error: ingreso mal el dato\n\n");
-            j=0;
-                 }
-    else  j =1;
-}
-while( j==0);
+    do{
+        while(getchar()!='\n');  /*limpieza de buffer */
 
-do{
-    while(getchar()!='\n');
-    fprintf(stderr,"%s",MSJ_CANT_MUESTRAS);
-    j=scanf("%d",&cant_muestras);
-    if( j==0 && cant_muestras<=0){
-            fprintf(stderr,"error: ingreso mal el dato\n");
+        fprintf(stderr,"%s",MSJ_T_INICIAL);
+        j=scanf("%f",&t_inicial);
+
+        if( j==0 ){
+                    fprintf(stderr,"error: ingreso mal el dato\n");
                  }
-    else  j =1;
-}
-while( j==0);
-do{
-    while(getchar()!='\n');
-    fprintf(stderr,"%s",MSJ_PRECISION);
-    j=scanf("%d",&precision);
-    if( j==0 || precision<0 ){
-            fprintf(stderr,"error: ingreso mal el dato\n");
-                 }
-    else  j =1;
-}
-while( j==0);
+        else  j =1;
+        }
+    while( j==0);  /* garantizo datos correctos */
+
+
+
+        /*al salir del ciclo se obtendra un valor de tiempo mayor o igual a cero ya que el tiempo*/
+        /* se ingresan el tiempo inicial y final con los que se trabajara*/
+
+    do{
+        while(getchar()!='\n');
+
+        fprintf(stderr,"%s",MSJ_T_FINAL);
+        j=scanf("%f",&t_final);
+
+        if( j==0 || t_final<=t_inicial ){
+                                fprintf(stderr,"error: ingreso mal el dato\n\n");
+                                j=0;
+                                }
+        else  j =1;
+        }
+
+    while( j==0);
+
+
+    do{
+        while(getchar()!='\n');
+
+        fprintf(stderr,"%s",MSJ_CANT_MUESTRAS);
+        j=scanf("%d",&cant_muestras);
+
+        if( j==0 && cant_muestras<=0){
+                    fprintf(stderr,"error: ingreso mal el dato\n");
+                     }
+
+        else  j =1;
+        }
+    while( j==0);
+
+
+    do{
+        while(getchar()!='\n');
+
+        fprintf(stderr,"%s",MSJ_PRECISION);
+        j=scanf("%d",&precision);
+
+        if( j==0 || precision<0 ){
+                    fprintf(stderr,"error: ingreso mal el dato\n");
+                    }
+        else  j =1;
+        }
+    while( j==0);
+
+
 
 /*---MENU DE FUNCIONES---*/
-fprintf(stderr,"%s\n\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n\n", MSJ_MENU_2, MSJ_SENOIDAL,MSJ_LOGARITMICA, MSJ_LOGLINEAL,MSJ_EXPONENCIAL,
+
+
+    fprintf(stderr,"%s\n\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n\n", MSJ_MENU_2, MSJ_SENOIDAL,MSJ_LOGARITMICA, MSJ_LOGLINEAL,MSJ_EXPONENCIAL,
         MSJ_ESCALON_HEAVISIDE,MSJ_MRUA,MSJ_PARABOLOIDE_HIPERBOLICO, MSJ_VOLVER);
 
-scanf("%i", &opciones) ;
+    scanf("%i", &opciones) ;
 
 
-switch (opciones) {
+    switch (opciones) {
 
-    case SALIR: {
-            repetir=0;
-            break;
+        case SALIR: {
+                    repetir=0;
+                    break;
                 }
-    case Fun_Senoidal: {
+        case Fun_Senoidal: {
 
                         generador_muestras (v, t, cant_muestras,Fun_Senoidal, t_inicial, t_final, precision);
 
                         break;
                         }
 
-    case Fun_Log:   {
+        case Fun_Log:   {
                         printf("\t T\t\tValor\n\n");
                         generador_muestras (v, t, cant_muestras,Fun_Log, t_inicial, t_final, precision);
 
                         break;
                     }
-    case Fun_Loglineal: {    printf("\t T\t\tValor\n\n");
+
+        case Fun_Loglineal: {
+                            printf("\t T\t\tValor\n\n");
                             generador_muestras (v, t, cant_muestras,Fun_Loglineal, t_inicial, t_final, precision);
-
-
 
                             break;
                         }
 
 
-    case Fun_Exp: {
-                    printf("\t T\t\tValor\n\n");
-                    generador_muestras (v, t, cant_muestras,Fun_Exp, t_inicial, t_final, precision);
-
-                    break;
-                    }
-    case Fun_escalon: {
+        case Fun_Exp: {
                         printf("\t T\t\tValor\n\n");
-                        generador_muestras (v, t, cant_muestras,Fun_escalon, t_inicial, t_final, precision);
+                        generador_muestras (v, t, cant_muestras,Fun_Exp, t_inicial, t_final, precision);
 
                         break;
-                        }
-    case Fun_mrua: {
-
-                      generador_muestras (v, t, cant_muestras,Fun_mrua, t_inicial, t_final, precision);
-
-                     break;
-
-                   }
-
-    case Fun_phiper: {
-                    printf("fun 7\n\n") ;
-                    break;
                     }
 
-    default:
-        fprintf(stderr,"error: ingreso mal la opcion\n");
 
-}
+        case Fun_escalon: {
+                            printf("\t T\t\tValor\n\n");
+                            generador_muestras (v, t, cant_muestras,Fun_escalon, t_inicial, t_final, precision);
 
-}while(repetir==0);
+                            break;
+
+                        }
+        case Fun_mrua: {
+
+                            generador_muestras (v, t, cant_muestras,Fun_mrua, t_inicial, t_final, precision);
+
+                            break;
+                    }
+
+        case Fun_phiper: {
+                            generador_muestras (v, t, cant_muestras,Fun_phiper, t_inicial, t_final, precision);
+                            break;
+                        }
+
+        default:
+                {fprintf(stderr,"error: ingreso mal la opcion\n");
+
+                 return EXIT_FAILURE ;
+                }
+
+        }
+
+    }
+        while(repetir==0);
 
 
-return EXIT_SUCCESS;
-}
+    return EXIT_SUCCESS;
+    }
 
 
 /**************/
@@ -232,6 +264,8 @@ void generador_muestras (float v[], float t[], int cant_muestras, int fun,float 
 
  float AMP, FASE, FREC ;
  float vel, acel, posicion ;
+ float rama_2, a, b , z;
+ float var_z;
 
 
 
@@ -244,9 +278,9 @@ for(i=0; i<cant_muestras; i++)
     switch (fun)
                 {
                     case Fun_Senoidal: {  if ( aux==0)
-                                              {  //printf( "usted esta a punto de cargar datos seno \n");
+                                              {
                                                  fprintf(stderr,"%s\n\n%s\n\n\n\n",FUNCION_A_MUESTREAR,FUNCION_SENOIDAL);
-                                                // printf("2pi: %f\n", 2*pi) ;
+
 
                                                  AMP= cargar_seno(dato_amp) ;
                                                  FASE= cargar_seno(dato_fase);
@@ -295,7 +329,7 @@ for(i=0; i<cant_muestras; i++)
                                         }
 
                     case Fun_mrua: {   if ( aux==0)
-                                              {  //printf( "usted esta a punto de cargar datos seno \n");
+                                              {
                                                  fprintf(stderr,"%s\n\n%s\n\n\n\n",FUNCION_A_MUESTREAR,FUNCION_MRUA);
 
 
@@ -315,8 +349,29 @@ for(i=0; i<cant_muestras; i++)
 
 
                                     }
-                    case Fun_phiper: {
-                                        printf("fun 7\n\n") ;
+                    case Fun_phiper: {   if ( aux==0)
+                                              {
+                                                 fprintf(stderr,"%s\n\n%s\n\n\n\n",FUNCION_A_MUESTREAR,FUNCION_HIPERBOLICA);
+
+                                                 fprintf (stderr, "%s",VALOR_Z ) ;
+                                                 z= cargar_parab ();
+
+                                                 fprintf (stderr, "%s",VALOR_A ) ;
+                                                 a= cargar_parab ();
+
+                                                 fprintf (stderr, "%s",VALOR_B ) ;
+                                                 b= cargar_parab ();
+
+                                                 aux=1 ;
+
+                                                printf("\t T\t\tValor\n\n");
+                                              }
+
+                                        v[i]= funcion_phiper(dato, z, a , b) ;
+
+                                        rama_2= (-1)*v[i] ;
+                                        printf("\t%.*f%20.*f%20.*f\n",  precision, t[i],  precision,  v[i], precision, rama_2 );
+
                                         break;
                                     }
 }
@@ -425,9 +480,8 @@ switch (datos)
 float funcion_senoidal (float dato, float AMP, float FASE, float FREC)
 
 {
-//printf( "dato*2pi frec :  %f\n", dato*2*pi*FREC);
-// printf( "seeeeeeeenoo   %f\n", AMP* sin( (dato*2*pi*FREC)+ FASE ));
-return  AMP*sin( dato*2*pi*FREC+ FASE ) ;
+
+    return  AMP*sin( dato*2*pi*FREC+ FASE ) ;
 
 
 
@@ -453,6 +507,7 @@ float cargar_mrua (t_mrua dato )
                                 else  j =1;
                             }
                         while( j==0);
+
                         return variable;
                         break;
                     }
@@ -503,33 +558,42 @@ float  funcion_mrua(float dato,float  x,float v,float a)
 
     return (x+(v*dato+(a)*0.5*(pow(dato,2)))) ;
 
+}
 
+
+/************ cargar datos hiperbolica *********/
+
+
+float cargar_parab ()
+
+{ float z;
+  int j;
+
+
+  do{
+       while(getchar()!='\n');
+
+       fprintf(stderr, "\n\n%s  ",  MSJ_VALOR);
+       j=scanf("%f",&z);
+
+       if( j==0 ){
+                  fprintf(stderr,"Error: ingreso mal el valor\n\n");
+                  }
+       else  j =1;
+    }
+   while( j==0);
+
+
+     return z;
 
 
 }
-/*
-void mrua(double tiempoi, double tiempof, double espacio, int prec)
+/************* funcion hiperbolica *********/
+
+float funcion_phiper(float dato, float z, float a, float b)
+
 {
 
-        float x_in, v_in, acel;
-        float tm;
-        float mrua;
 
-        printf("%s",MSJ_MRUA_POS);
-        scanf("%f",&x_in);
-
-        printf("%s",MSJ_MRUA_VEL);
-        scanf("%f",&v_in);
-
-        printf("%s",MSJ_MRUA_ACEL);
-        scanf("%f",&acel);
-
-        for(tm=tiempoi; tm<tiempof; tm+=espacio){
-
-        mrua=((x_in)+(v_in)*tm+(acel)*0.5*(pow(tm,2)));
-
-        printf("%f:%.*f\n",tm, prec, mrua);
-    }
-    return;
+    return (sqrt( pow(dato*(b/a), 2) - z* pow(b, 2)) );
 }
-*/
