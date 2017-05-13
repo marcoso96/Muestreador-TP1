4d82ea53ddf31ccb044e5483f0ebf5ef38887d41
mikeila95 <micaak14@gmail.com>
2017-05-13 08:05:03 +0000

    Create macrosranking.h

---
+++ macrosranking.h
@@ -0,0 +1,5 @@
+#define MSJ_ERROR "Error"
+#define MSJ_ERROR_BAJA "Falla en archivo a dar de baja. EL archivo no esta ordenado o se quiere dar de baja un registro inexistente en la base de dato"
+#define MSJ_ERROR_ARG "Falla en validacion de argumentos"
+#define MSJ_ERROR_ALTA "Falla en archivo a dar de alta. Existen registros duplicados"
+#define MSJ_ERROR_MODIFICACIONES "Falla en archivo a modificar. Registros a modificar no existen"
