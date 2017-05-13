78128c767110bca7f671be18c5af39f652844962
marcoso96 <marcosobando96@hotmail.com>
2017-05-13 10:44:27 -0300

    Add files via upload

---
+++ constantes_crearbase.h
@@ -0,0 +1,15 @@
+#define MAX_STR 200
+#define INIT_CHOP 1     /*Cuanto les parece razonable ponerle a esta cantidad? Seria la cantidad max de libros que se van a ingresar en un principio*/
+#define CHOP_SIZE 1
+#define ID_FIELD_POS 0
+/*crar base argumentos*/
+#define ARG_POS_1 1
+#define ARG_POS_2 2
+
+#define TITULO_FIELD_POS 1
+#define AUTOR_FIELD_POS 2
+#define GENERO_FIELD_POS 3
+#define FECHA_FIELD_POS 4
+#define PUNTAJE_FIELD_POS 5
+#define REVIEWS_FIELD_POS 6
+#define CANT_ARGUMENTOS 3
---
+++ macros_crearbase.h
@@ -0,0 +1,5 @@
+#define MSJ_ERROR "Error"
+#define MSJ_ERROR_NOMEM "No hay memoria disponible"
+/*en otro archivo de delimitadores*/
+#define DELIM_INI ','
+#define DELIM_BARRA_CERO '\0'
