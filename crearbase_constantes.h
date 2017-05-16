d89ecfb4bcb794b9c234a8feb5f0ec93698dc9b3
marcoso96 <marcosobando96@hotmail.com>
2017-05-16 00:16:52 -0300

    Add files via upload

---
+++ crearbase_constantes.h
@@ -0,0 +1,25 @@
+#define MAX_LINE 100 
+#define INIT_CHOP 1     /*Cuanto les parece razonable ponerle a esta cantidad? Seria la cantidad max de libros que se van a ingresar en un principio*/
+#define CHOP_SIZE 1
+#define ID_FIELD_POS 0
+/*crar base argumentos*/
+#define ARG_POS_1 1
+#define ARG_POS_2 2
+
+#define FIELD_NUM 6
+#define TITULO_FIELD_POS 1
+#define AUTOR_FIELD_POS 2
+#define GENERO_FIELD_POS 3
+#define FECHA_FIELD_POS 4
+#define PUNTAJE_FIELD_POS 4
+#define REVIEWS_FIELD_POS 5
+#define CANT_ARGUMENTOS 3
+
+#define MAX_DIGIT_ID 5
+#define MAX_TITLE 30
+#define MAX_AUTOR 30
+#define MAX_GENERO 15
+#define DATE_LENGTH 11
+#define PUNTAJE_LENGTH 3
+#define REVIEWS_MAX 5
+
---
+++ crearbase_macros.h
@@ -0,0 +1,10 @@
+#define MSJ_ERROR "Error"
+#define MSJ_ERROR_NOMEM "No hay memoria disponible"
+#define MSJ_ERROR_ARG "Argumentos erroneos"
+#define MSJ_ERROR_LINE "Linea del archivo con formato incorrecto"
+#define MSJ_ERROR_WRITE "Error de escritura"
+#define MSJ_ERROR_FILE "Error de archivo"
+#define MSJ_ERROR_NULL_PTR "Error de puntero nulo"
+/*en otro archivo de delimitadores*/
+#define DELIM_INI ','
+#define DELIM_BARRA_CERO '\0'
